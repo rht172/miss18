0d3a6aa6ce22202e26c702806b231eb2b7050882
Rohit  Singh Rajpoot <Rohit.SinghRajpoot@go-mmt.com>
2025-05-20 23:52:27 +0530

    password change

--- includes/constants.php
+++ includes/constants.php
@@ -2,6 +2,6 @@
 // Database Constants
 define("DB_SERVER", "127.0.0.1");
 define("DB_USER", "root");
-define("DB_PASS", "4222918859");
+define("DB_PASS", "Rohit91$");
 define("DB_NAME", "miss_18_ecom");
 ?>
